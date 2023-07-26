727bc45465050016abde28b214876101a05f7d60
5tepan <f.stepa.n@yandex.ru>
2023-07-26 20:24:54 +0300

    observer

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import "../styles/App.css"
 import {usePosts} from "../hooks/usePosts"
 import PostService from "../api/PostService"
@@ -11,6 +11,8 @@ import PostFilter from "../components/PostFilter"
 import Loader from "../components/ui/loader/Loader"
 import PostList from "../components/PostList"
 import Pagination from "../components/ui/pagination/Pagination"
+import {useObserver} from "../hooks/useObserver";
+import MySelect from "../components/ui/select/MySelect";
 
 
 function Posts() {
@@ -20,12 +22,13 @@ function Posts() {
     const [totalPages, setTotalPages] = useState(0)
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(10)
+    const lastElement = useRef()
 
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
         const response = await PostService.getAll(limit, page)
-        setPosts(response.data)
+        setPosts([...posts, ...response.data])
         const totalCount = response.headers['x-total-count']
         setTotalPages(getPageCount(totalCount, limit))
     })
@@ -39,9 +42,13 @@ function Posts() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+        setPage(page + 1)
+    })
+
     useEffect(() => {
-        fetchPosts()
-    }, [page])
+        fetchPosts(limit, page)
+    }, [page, limit])
 
     const changePage = (page) => {
         setPage(page)
@@ -63,18 +70,32 @@ function Posts() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            <MySelect
+                value={limit}
+                onChange={value => setLimit(value)}
+                defaultValue='Count elements on page'
+                options={[
+                    {value: 5, name: '5'},
+                    {value: 10, name: '10'},
+                    {value: 25, name: '25'},
+                    {value: -1, name: 'All'},
+                ]}
+            />
             {postError &&
                 <h1>Error ${postError}</h1>
             }
-            {isPostsLoading
-                ? <div style={{display: "flex", justifyContent: "center", marginTop: "10px"}}>
+            <PostList
+                remove={removePost}
+                posts={sortedAndSearchedPosts}
+                title={'Posts List'}
+            />
+            <div ref={lastElement} style={{height: 20, background: "red"}}>
+
+            </div>
+            {isPostsLoading &&
+                <div style={{display: "flex", justifyContent: "center", marginTop: "10px"}}>
                     <Loader/>
                 </div>
-                : <PostList
-                    remove={removePost}
-                    posts={sortedAndSearchedPosts}
-                    title={'Posts List'}
-                />
             }
             <Pagination
                 page={page}
